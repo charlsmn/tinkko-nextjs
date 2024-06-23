dd4519bab01d09e8980e2f80053131944b66678a
Charls <carlos.mancilla993@gmail.com>
2024-06-22 21:52:38 -0500

    login

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,45 @@
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import db from '@/lib/db'
+import bcrypt from 'bcrypt'
+
+const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: 'Email', type: 'email' },
+                password: { label: 'Password', type: 'password' },
+            },
+            async authorize(credentials, req) {
+                const userFound = await db.user.findUnique({
+                    where: {
+                        email: credentials?.email,
+                    },
+                })
+
+                if (!userFound) throw new Error('Usuario no encontrado')
+
+                const matchPassword = await bcrypt.compare(
+                    credentials.password,
+                    userFound.password
+                )
+
+                if (!matchPassword) throw new Error('Contraseña incorrecta')
+
+                return {
+                    id: userFound.id,
+                    email: userFound.email,
+                    name: userFound.firstName,
+                }
+            },
+        }),
+    ],
+    pages: {
+        signIn: '/auth/login',
+    },
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+'use client'
+import { signOut } from 'next-auth/react'
+
+export default function Dashboard() {
+    return <button onClick={() => signOut()}>Cerrar sesión</button>
+}
