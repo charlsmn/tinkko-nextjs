18f166237c9fab3e80954fb9adb5ae656cbcacfe
Charls <carlos.mancilla993@gmail.com>
2024-06-23 22:49:29 -0500

    authOptions

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,46 +1,6 @@
 import NextAuth from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import db from '@/lib/db'
-import bcrypt from 'bcrypt'
 
-export const authOptions = {
-    providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                email: { label: 'Email', type: 'email' },
-                password: { label: 'Password', type: 'password' },
-            },
-
-            async authorize(credentials, req) {
-                const userFound = await db.user.findUnique({
-                    where: {
-                        email: credentials?.email,
-                    },
-                })
-
-                if (!userFound) throw new Error('Usuario no encontrado')
-
-                const matchPassword = await bcrypt.compare(
-                    credentials.password,
-                    userFound.password
-                )
-
-                if (!matchPassword) throw new Error('Contraseña incorrecta')
-
-                return {
-                    id: userFound.id,
-                    email: userFound.email,
-                    name: userFound.firstName,
-                }
-            },
-        }),
-    ],
-    pages: {
-        signIn: '/login',
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-}
+import { authOptions } from '@/lib/authOptions'
 
 const handler = NextAuth(authOptions)
 
--- src/components/userInfo.tsx
+++ src/components/userInfo.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/lib/authOptions'
 
 export default async function UserInfo() {
     const session = await getServerSession(authOptions)
